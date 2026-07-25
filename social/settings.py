7a64d38d2c5f271ce3f8114db6f6737f7eaf2b38
vince09-G <wonderfulkadani09@gmail.com>
2026-07-25 17:01:36 +0200

    Refine dependencies and update Django settings configuration

--- social/settings.py
+++ social/settings.py
@@ -23,12 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-qf*c96bkv-&eoppcon$i=pu6ns^jop)aut6gxd7b%+2v@akzu&')
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
+#default='django-insecure-qf*c96bkv-&eoppcon$i=pu6ns^jop)aut6gxd7b%+2v@akzu&'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
-
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+DEBUG =os.environ.get('DEBUG', 'False').lower() == 'true'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
@@ -85,6 +84,8 @@ DATABASES = {
     )
 }
 
+database_url= os.environ.get('DATABASE_URL')
+DATABASES['default']= dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators

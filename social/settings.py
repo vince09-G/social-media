8935c72923ef3b8147ee77cf227597fc26dd3b4b
vince09-G <wonderfulkadani09@gmail.com>
2026-07-27 14:40:02 +0200

    Update project files and configuration

--- social/settings.py
+++ social/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 #default='django-insecure-qf*c96bkv-&eoppcon$i=pu6ns^jop)aut6gxd7b%+2v@akzu&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =os.environ.get('DEBUG', 'False').lower() == 'true'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
